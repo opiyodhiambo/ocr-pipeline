e753a9dbbac765eb5c56aca6a56dba427773fc5e
opiyodhiambo <rnldlinus@gmail.com>
2023-11-26 10:58:03 +0300

    secure secrets

--- src/main/kotlin/com/adventure/ocrpipeline/service/S3Service.kt
+++ src/main/kotlin/com/adventure/ocrpipeline/service/S3Service.kt
@@ -4,6 +4,8 @@ import com.amazonaws.services.s3.AmazonS3
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.BodyInserters
+import java.io.File
 
 @Service
 class S3Service(private val digitalOceanClient: AmazonS3) {
@@ -11,11 +13,16 @@ class S3Service(private val digitalOceanClient: AmazonS3) {
     @Value("\${digital-ocean.bucket-name}")
     private val bucketName: String? = null
 
-    fun uploadDocument() {
+    fun uploadDocument( file: File) {
         TODO()
     }
 
-    fun downloadDocument() {
-        TODO()
+
+    fun downloadDocument(fileName: String) {
+
+
     }
 }
+//
+//https://tajji-kyc-documentation-bucket.ams3.digitaloceanspaces.com
+//https://tajji-kyc-documentation-bucket.ams3.digitaloceanspaces.com/KE-KRA-PIN-CERTIFICATE/A012203309Y.pdf
